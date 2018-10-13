1849d8885a41370d7d88d46a37a4b35459a0ad6c
ohyecloudy <ohyecloudy@gmail.com>
2018-10-14 08:13:46 +0900

    :tada: mix new --sup

---
+++ lib/search_ranking_telegram.ex
@@ -0,0 +1,18 @@
+defmodule SearchRankingTelegram do
+  @moduledoc """
+  Documentation for SearchRankingTelegram.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SearchRankingTelegram.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/search_ranking_telegram_test.exs
@@ -0,0 +1,8 @@
+defmodule SearchRankingTelegramTest do
+  use ExUnit.Case
+  doctest SearchRankingTelegram
+
+  test "greets the world" do
+    assert SearchRankingTelegram.hello() == :world
+  end
+end
